c706b27258b1d3d4376663439fd47ed8add73851
Faraz Jahangir <38138258+Farazjahangir@users.noreply.github.com>
2018-08-06 12:45:18 -0700

    Add files via upload
    Assignment done

---
+++ js/app.js
@@ -0,0 +1,57 @@
+
+var tabelEl = document.getElementById("table");
+
+
+function getData(){
+fetch("https://api.github.com/users/izaanjahangir/followers")
+.then((res)=>{
+    return res.json()
+})
+.then((myRes)=>{
+    console.log("Data from Api = " , myRes);
+    tabelEl.innerHTML = ""
+    for(var i =0 ; i < myRes.length; i++){
+        renderDataOnDom(myRes[i].login , myRes[i].id)
+    }   
+})
+
+
+caches.match('https://api.github.com/users/izaanjahangir/followers')
+.then((res)=>{
+    if(!res){
+        console.log("data Not Found")
+    }    
+    return res.json()  
+    })
+    .then((myData)=>{
+        console.log("Data From Cache = " , myData);
+        tabelEl.innerHTML = ""
+        for(var i =0 ; i < myData.length; i++){
+            renderDataOnDom(myData[i].login , myData[i].id)
+        }   
+})
+    .catch(()=>{
+        console.log("error");
+        
+    })
+}
+
+
+function renderDataOnDom(loginName , id){
+    tabelEl.innerHTML += `
+        <tr>
+            <td>${loginName}</td>
+            <td>${id}</td>
+        </tr>
+    `
+
+}
+
+if("serviceWorker" in navigator){
+    navigator.serviceWorker
+    .register("/sw.js")
+    .then(()=>{
+        console.log("/Service Worker Registered Successfully");        
+    })
+    
+}
---
+++ sw.js
@@ -0,0 +1,61 @@
+var cacheName = "static-files_v1"
+var filesToCache = [
+    "/",
+    "/index.html",
+    "/css/bootstrap.min.css",
+    "/css/style.css",
+    "/js/app.js"
+]
+
+
+self.addEventListener("install" , (e)=>{
+    console.log("Service Worker Installed");
+    e.waitUntil(
+        caches.open(cacheName).then((cache)=>{
+            cache.addAll(filesToCache)
+            console.log("files Cached Successfully");
+            
+        })
+    )
+})
+
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.log('[ServiceWorker] Removing old cache', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+    return self.clients.claim();
+  });
+
+
+
+  self.addEventListener('fetch',(e)=>{
+
+    console.log(e.request.url)
+    if(e.request.url === 'https://api.github.com/users/izaanjahangir/followers'){
+        fetch(e.request)
+        .then(response => {
+            return caches.open("Dynamic")
+                .then(cache => {
+                    console.log('Caching new data...')
+                    cache.put(e.request.url,response.clone());
+                    return response;
+                })
+        })
+    }else{
+        e.respondWith(
+            caches.match(e.request).then(function(response) {
+                return response || fetch(e.request).catch(err => console.log(err));
+            })
+        )
+    }
+})
+  
